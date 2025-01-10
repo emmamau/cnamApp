343dc9261413a02662ba5aa8bc3a38f97b55ff8f
Darkpaul12 <45434617+darkpaul12@users.noreply.github.com>
2025-01-10 17:52:06 +0100

    Update config.js
    Signed-off-by: Darkpaul12 <45434617+darkpaul12@users.noreply.github.com>

--- deploy/api/config.js
+++ deploy/api/config.js
@@ -1,10 +1,11 @@
-module.exports =  {
-    ACCESS_TOKEN_SECRET : "EMMA123",
-    BDD : {
-    "host" :"dpg-ctsigpi3esus73dqmjog-a.frankfurt-postgres.render.com",
-    "port" : "",
-    "user" : "postgrepaul_omr5_user",
-    "password" : "Tdcejkao8o0tYfKl3xQ6iR46H9ekpBpX",
-    "bdname" :"postgrepaul_omr5" 
+
+module.exports = {
+    ACCESS_TOKEN_SECRET: "EMMA123",
+    BDD: {
+        "host": "dpg-ctsigpi3esus73dqmjog-a.frankfurt-postgres.render.com",
+        "port": "",
+        "user": "postgrepaul_omr5_user",
+        "password": "Tdcejkao8o0tYfKl3xQ6iR46H9ekpBpX",
+        "bdname": "postgrepaul_omr5"
     }
 }
