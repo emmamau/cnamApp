257af52cf7d86301251998b99aec354d279f3ae4
Darkpaul12 <darkpaul12@laposte.net>
2025-01-10 17:35:19 +0100

    Update config.js

--- deploy/api/config.js
+++ deploy/api/config.js
@@ -2,7 +2,7 @@ module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
     "host" :"dpg-ctsigpi3esus73dqmjog-a.frankfurt-postgres.render.com",
-    "port" : "5432",
+    "port" : "",
     "user" : "postgrepaul_omr5_user",
     "password" : "Tdcejkao8o0tYfKl3xQ6iR46H9ekpBpX",
     "bdname" :"postgrepaul_omr5" 
