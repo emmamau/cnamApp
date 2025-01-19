9cd153c3802396bbe4a92f04b86e35345e97a684
Darkpaul12 <darkpaul12@laposte.net>
2025-01-19 15:14:25 +0100

    update

--- deploy/api/routes/jwtMiddleware.js
+++ deploy/api/routes/jwtMiddleware.js
@@ -3,13 +3,10 @@ const { ACCESS_TOKEN_SECRET } = require("../config.js");
 
 module.exports = {
      checkJwt : (req, res, next) => {
-    // Get the JWT from the request header.
     const token = req.headers['authorization'];
     let jwtPayload;
   
-    // Validate the token and retrieve its data.
     try {
-        // Verify the payload fields
         let jwtBearer = token.split(' ')[1];
         console.log ("Authorization: " + jwtBearer);
         jwtPayload = jwt.verify(jwtBearer, ACCESS_TOKEN_SECRET ,
@@ -21,7 +18,6 @@ module.exports = {
           ignoreNotBefore: false
       }
         );
-        // Add the payload to the request so controllers may access it.
         req.token = jwtPayload;
     } catch (error) {
        console.log (error);
@@ -31,7 +27,6 @@ module.exports = {
         return;
     }
   
-    // Pass programmatic flow to the next middleware/controller.
     next();
   }
 }
